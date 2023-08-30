e331fb65eea9d0fceb77bb37ace1f0f81f0333ac
Amir Hamza <hamza_amir@yahoo.com>
2023-08-31 01:45:55 +0600

    7.Section 2 - Register ApplicationDbContext to program class file

--- WhiteLagoon_web/Program.cs
+++ WhiteLagoon_web/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
