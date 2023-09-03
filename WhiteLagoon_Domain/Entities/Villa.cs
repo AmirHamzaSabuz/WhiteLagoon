49c50e99eccafc6bae58674c45a2eed7ea396ba7
Amir Hamza <hamza_amir@yahoo.com>
2023-09-03 07:47:36 +0600

    21.Section 3 - Display Data Annotation

--- WhiteLagoon_Domain/Entities/Villa.cs
+++ WhiteLagoon_Domain/Entities/Villa.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WhiteLagoon_Domain.Entities
 {
 	public class Villa
 	{
 		public int Id { get; set; }
+
 		public required string Name { get; set; }
+
 		public string? Description { get; set; }
-		public double Price { get; set; }
+
+        [Display(Name = "Price per night")]
+        public double Price { get; set; }
+
 		public int Sqft { get; set; }
+
 		public int Occupancy { get; set; }
-		public string? ImageUrl { get; set; }
+
+        [Display(Name = "Image Url")]
+        public string? ImageUrl { get; set; }
+
 		public DateTime? Created_Date { get; set; }
+
 		public DateTime? Updated_Date { get; set; }
 	}
 }
