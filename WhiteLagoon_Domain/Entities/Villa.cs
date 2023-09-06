7c218a7feb63be9e7b6dbaa181f3bddda9f35bb6
Amir Hamza <hamza_amir@yahoo.com>
2023-09-06 23:36:33 +0600

    More Data Annotations

--- WhiteLagoon_Domain/Entities/Villa.cs
+++ WhiteLagoon_Domain/Entities/Villa.cs
@@ -13,20 +13,20 @@ namespace WhiteLagoon_Domain.Entities
 		public int Id { get; set; }
 
 		[MaxLength(50)]
-        public required string Name { get; set; }
+		public required string Name { get; set; }
 
 		public string? Description { get; set; }
 
-        [Display(Name = "Price per night")]
-        [Range(10, 10000)]
-        public double Price { get; set; }
+		[Display(Name = "Price per night")]
+		[Range(10, 10000)]
+		public double Price { get; set; }
 
 		public int Sqft { get; set; }
 
-        [Range(1, 10)]
-        public int Occupancy { get; set; }
+		[Range(1, 10)]
+		public int Occupancy { get; set; }
 
-        [Display(Name = "Image Url")]
+		[Display(Name = "Image Url")]
 		public string? ImageUrl { get; set; }
 
 		public DateTime? Created_Date { get; set; }
