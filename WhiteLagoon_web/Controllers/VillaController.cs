4d100741ce0d46440b3fe4d81c2a472434140dd0
Amir Hamza <hamza_amir@yahoo.com>
2023-09-06 07:36:54 +0600

    23.Section 3 - Server Side Validation

--- WhiteLagoon_web/Controllers/VillaController.cs
+++ WhiteLagoon_web/Controllers/VillaController.cs
@@ -27,9 +27,13 @@ namespace WhiteLagoon_web.Controllers
         [HttpPost]
         public IActionResult Create(Villa obj)
         {
-            _db.Villas.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
